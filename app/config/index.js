84a1cbb49eee7d4adfe07ea2edd9570807db717f
Liễu Hoàng Duy <112844257+duyb1906441@users.noreply.github.com>
2023-01-16 11:23:53 +0700

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
+        uri: process.env.MONGODB_URI || "mongodb+srv://duyb1906441:lieuhoangduy909@cluster0.l5jeijx.mongodb.net/?retryWrites=true&w=majority"
     }
 };
 
